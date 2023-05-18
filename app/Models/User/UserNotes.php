ff8c7410824dfc3d1283d2fe4ec2461b637fd6ab
Leonard-Vrana <94246799+Leonard-Vrana@users.noreply.github.com>
2023-05-18 08:48:29 +0200

    Update

---
+++ app/Models/User/UserNotes.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models\User;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class UserNotes extends Model
+{
+    use HasFactory;
+}
